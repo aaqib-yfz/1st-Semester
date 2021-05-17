78cf3902afaeb000918f5e7cda29c8192fc9350d
aaqib-yfz <aaqibshaheer2001@gmail.com>
2021-05-17 08:56:10 -0700

    updated

---
+++ reverse_number.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int number ;
+cout <<"Enter 5 Digit Number =";
+cin >> number;
+int temp = number;
+
+int digit =0;
+int rev=0;
+
+digit=number%10;
+rev= rev*10 + digit;
+number/=10;
+
+digit=number%10;
+rev= rev*10 + digit;
+number/=10;
+
+digit=number%10;
+rev= rev*10 + digit;
+number/=10;
+
+digit=number%10;
+rev= rev*10 + digit;
+number/=10;
+
+digit=number%10;
+rev= rev*10 + digit;
+number/=10;
+
+cout << "Reverse of the " << temp << " is : " <<rev;
+return 0;
+}
