349b4df3a59110d36ef1c39d5f4cd536a7feedbf
aaqib-yfz <aaqibshaheer2001@gmail.com>
2021-08-24 08:30:32 -0700

    random

--- reverse_number.cpp
+++ reverse_number.cpp
@@ -9,26 +9,11 @@ int temp = number;
 
 int digit =0;
 int rev=0;
-
+while(number>0){
 digit=number%10;
 rev= rev*10 + digit;
 number/=10;
-
-digit=number%10;
-rev= rev*10 + digit;
-number/=10;
-
-digit=number%10;
-rev= rev*10 + digit;
-number/=10;
-
-digit=number%10;
-rev= rev*10 + digit;
-number/=10;
-
-digit=number%10;
-rev= rev*10 + digit;
-number/=10;
+}
 
 cout << "Reverse of the " << temp << " is : " <<rev;
 return 0;
